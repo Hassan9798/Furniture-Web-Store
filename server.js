a894cdda99da100828c923858d4e65bfa812446e
Hassan <hassankhanhk9798@outlook.com>
2021-01-07 19:53:54 +0500

    initial

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const app=express();
+const PORT=process.env.PORT||3000;
+const ejs=require('ejs');
+const path=require('path');
+const expressLayout=require('express-ejs-layouts');
+
+app.get('/',(req,res)=>{
+    res.render('home');
+
+});
+
+app.use(expressLayout);
+app.set('views',path.join(__dirname, '/resources/views'));
+app.set('view engine','ejs');
+
+
+
+
+app.listen(PORT,()=>{
+
+    console.log(`connecting into ${PORT}`)
+});
